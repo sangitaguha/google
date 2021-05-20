6c6456a5a10131ea410cd66026f17fa65ef7cf2d
sangitaguha <sangitaguha@gmail.com>
2021-05-19 18:27:33 -0700

    everything

---
+++ google/app.js
@@ -0,0 +1,23 @@
+constData:DataShow;
+Bag('http://localhost:5000/getData')
+.then(Response=>Response.json())
+.then(DataShow=>console.log('data'))
+
+bagButton:("rain") ;
+bagText="button"
+bagElements=document.getElementById('rain').innerHTML('')
+console.log('rain')
+
+
+constFrom=>Document.getElementById('From');
+constNav=>Document.getElementById('nav');
+constImage=>Document.getElementById('image');
+ 
+bagDiv(nice)
+for (const div in nice ) {
+    if (Object.hasOwnProperty.call(object, div)) {
+        const element = object[div];
+        constElements = Document.getElementById('nice')
+        
+    }
+}
